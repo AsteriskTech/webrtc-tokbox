a83e7d6cab400323b21f81569b42a849dc5b5579
slavkur <slavkur2013@gmail.com>
2013-10-10 09:43:33 +0300

    created tokbox chatroom

--- web.js
+++ web.js
@@ -1,12 +1,38 @@
 var express = require("express");
-var app = express();
-app.use(express.logger());
+var OpenTokLibrary = require('opentok');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+var urlSessions = {};
+var apikey = '43672442';
+var secretkey = '2d109ea79c71e50d427fa39c3cea3b36ad1c33db';
+var OpenTokObject = new OpenTokLibrary.OpenTokSDK(apikey, secretkey);
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+function sendResponse( sessionId, responder ){
+  var token = OpenTokObject.generateToken({session_id: sessionId});
+  var data = {OpenTokKey:apikey, sessionId: sessionId, token:token};
+  responder.render('index', data);
+}
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+app.get("/chat/:root", function(req, res){
+  if (urlSessions[req.params.room] == undefined) {
+    OpenTokObject.createSession(function(sessionId){
+      urlSessions[req.params.room] = sessionId;
+      sendResponse(sessionId, res);
+    });
+  } else {
+    sessionId = urlSessions[req.params.room];
+    sendResponse(sessionId, res);
+  }
 });
